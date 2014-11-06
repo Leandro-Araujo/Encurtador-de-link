f5bbf8d7bd560f89142963631e0b3158add24d58
Leandro S. Araújo <leandro_sy@hotmail.com>
2014-11-06 03:09:05 -0200

    Criado o db.js

---
+++ model/db.js
@@ -0,0 +1,22 @@
+
+/*
+ * GET users listing.
+ */
+
+var Mongoose = require('mongoose');
+
+var db = Mongoose.connection;
+
+db.on('error', console.error);
+db.once('open', function() {
+
+});
+
+Mongoose.connect('mongodb://localhost/sitelink');
+
+var SiteSchema = new Mongoose.Schema({
+  site: String,
+  newLink: String
+});
+
+var Site = Mongoose.model('Site', SiteSchema);
